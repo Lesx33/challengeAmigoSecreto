fce512e8c08d134d0ff520c62c30f086e9bca1e8
Lesx33 <fernandez.leslya@gmail.com>
2025-08-08 14:29:13 -0600

    implementando la funcion sortearAmigo

--- app.js
+++ app.js
@@ -1,27 +1,36 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
-function agregarAmigo(){
+function agregarAmigo() {
     let nuevoAmigo = document.getElementById('amigo').value;
-    if(nuevoAmigo == ''){
+    if (nuevoAmigo == '') {
         alert('Por favor, inserte un nombre.');
-    } else{
+    } else {
         amigos.push(nuevoAmigo);
         document.getElementById('amigo').value = '';
-        actualizarLista(); 
+        actualizarLista();
     }
-    console.log(nuevoAmigo);
-    console.log(amigos);
 }
 
-function actualizarLista(){
+function actualizarLista() {
     let lista = document.getElementById('listaAmigos');
     lista.innerHTML = '';
-    for (const amigo of amigos){
+    for (const amigo of amigos) {
         console.log(amigo)
         let nuevoAmigo = document.createElement('li');
         nuevoAmigo.textContent = amigo;
         lista.appendChild(nuevoAmigo);
     }
-    
+
+}
+
+function sortearAmigo() {
+    let rango = amigos.length;
+    let amigo = document.getElementById('resultado');
+    if(rango == 0){
+        alert('Primero debes ingresar los nombres de tus amigos.');
+        return;
+    }
+    amigoSecreto = Math.floor(Math.random() * rango);
+    amigo.innerHTML = `El amigo secreto es: ${amigos[amigoSecreto]}`;
 }
